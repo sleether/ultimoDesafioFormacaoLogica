0150c22f311c77c121def7c8d826db616a450e39
Sleether <bruno_lucas_97@hotmail.com>
2024-04-15 15:58:37 -0300

    Desafio de classes de heroi DIO

---
+++ index.js
@@ -0,0 +1,44 @@
+//Primeiro a gente cria uma classe chamada Heroi//
+//Dentro dela montamos um constructor que vai agregar os dados do heroi//
+
+class Heroi {
+        constructor(nome, hp, tipo) {
+            this.nome = nome;
+            this.hp = hp;
+            this.tipo = tipo;
+        }
+
+
+//Hora de criar o comando de attack//
+
+attack() {
+    let attack;
+
+//Agora criaremos o tipo de ataque e o retorno da classe do Heroi de acordo com o ataque//
+switch(this.tipo){
+    case 'mago':
+        attack = 'magia'
+        break;
+    case 'guerreiro':
+        attack ='espada'
+        break;
+    case 'monge':
+        attack = 'artes marciais'
+        break;
+    case 'ninja':
+        attack = 'shuriken'
+        break;
+    default:
+        attack = 'ataque desconhecido';        
+}
+console.log(`O ${this.nome} , de ${this.hp} de HP, atacou usando ${attack}`);
+
+}
+}
+//Fechamos os comandos acima com as chaves e agora vamos pra parte legal//
+
+const meuHeroi = new Heroi('PatoDonald o Grande Mago Implacável', 2500,'mago');
+meuHeroi.attack();
+
+const outroHeroi = new Heroi('Gibaltrar', 3500, 'guerreiro');
+outroHeroi.attack();
